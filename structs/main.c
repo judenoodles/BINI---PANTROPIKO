4dbe32e8b7f94a3cef33859c00e6b424edb75f27
judenoodles <wandajdlc.528@gmail.com>
2024-03-12 21:38:10 +0800

    Initial Commit

---
+++ structs/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  structs
+//
+//  Created by jude on 3/12/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
