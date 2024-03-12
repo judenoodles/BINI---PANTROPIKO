1af633e5546f83117da76cef6480e4c2f34b43fe
judenoodles <wandajdlc.528@gmail.com>
2024-03-12 23:17:57 +0800

    boop

--- structs_practice.c
+++ structs_practice.c
@@ -3,23 +3,25 @@
 typedef char word[20];
 
 struct info {
-    word *place;
-    word *name;
-    word *transpo;
+    word place;
+    word name;
+    int age;
 } p1, p2;
 
 
 int main () {
     printf("enter name: ");
-    scanf("%s", *p1.name);
-    printf("enter the place you always go to: ");
-    scanf("%s", *p1.place);
-    printf("enter mode of transpo: ");
-    scanf("%s", *p1.transpo);
+    scanf("%s", p1.name);
+    printf("what do you like: ");
+    scanf("%s", p1.place);
+    printf("enter age: ");
+    scanf("%d", &p1.age);
     
-    printf("This is the final statement\n");
-    printf("Hi, my name is %s. I like %s. I always go there on a %s.\nAnd today is actually my birthday too :)", *p1.name, *p1.place, *p1.transpo);
+    
+    printf("This is the final statement:\n");
+    printf("Hi, my name is %s. I like %s. I'm %c years old.\nAnd today is actually my birthday too :)", p1.name, p1.place, p1.age);
     
     
     return 0;
+
 }
